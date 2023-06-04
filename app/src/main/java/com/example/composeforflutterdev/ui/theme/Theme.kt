f507c1be379a4895c487a5a62ffee27a765c29b4
Richard Dewan <richarddewan@hotmail.com>
2023-06-04 07:25:28 +0700

    lesson 7

--- app/src/main/java/com/example/composeforflutterdev/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composeforflutterdev/ui/theme/Theme.kt
@@ -1,10 +1,19 @@
 package com.example.composeforflutterdev.ui.theme
 
+import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 
 private val LightColors = lightColorScheme(
@@ -77,11 +86,33 @@ fun AppTheme(
   useDarkTheme: Boolean = isSystemInDarkTheme(),
   content: @Composable() () -> Unit
 ) {
-  val colors = if (!useDarkTheme) {
-    LightColors
-  } else {
-    DarkColors
-  }
+
+    val context = LocalContext.current
+    val colors = when {
+        (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) -> {
+            if (useDarkTheme) dynamicDarkColorScheme(context)
+            else dynamicLightColorScheme(context)
+        }
+        useDarkTheme -> DarkColors
+        else -> LightColors
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colors.secondary.toArgb()
+            WindowCompat
+                .getInsetsController(window,view)
+                .isAppearanceLightStatusBars = useDarkTheme
+        }
+    }
+
+//  val colors = if (!useDarkTheme) {
+//    LightColors
+//  } else {
+//    DarkColors
+//  }
 
   MaterialTheme(
     colorScheme = colors,
